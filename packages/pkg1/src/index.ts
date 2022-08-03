25257cc42016eac4a79f855ebb40a60ccf995c69
tim.li <tim.li@laxtongroup.com>
2022-08-03 15:20:55 +0800

    commit-pre test

--- packages/pkg1/src/index.ts
+++ packages/pkg1/src/index.ts
@@ -7,13 +7,3 @@ console.info(`${add(1, 2)}`)
 console.info(`${reduce(1, 2)}`)
 // eslint-disable-next-line no-console
 console.info(123)
-// eslint-disable-next-line no-console
-console.info(456)
-// eslint-disable-next-line no-console
-console.info(789)
-// eslint-disable-next-line no-console
-console.info(101112)
-// eslint-disable-next-line no-console
-console.info(131415)
-// eslint-disable-next-line no-console
-console.info(161718)
